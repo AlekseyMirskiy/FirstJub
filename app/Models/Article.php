fb5e8c59b78be70accdef3b05370203cb61d8bf8
AlekseyMirskiy <aleksey-mirskiy@yandex.ru>
2020-01-25 17:58:14 +0300

    new

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,14 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Article extends Model
 {
     protected $table = 'articles';
     protected $fillable = ['title', 'description', 'image_path', 'user_id', 'category_id'];
     
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'article_category', 'article_id', 'category_id', 'id', 'id');
     }
 }
